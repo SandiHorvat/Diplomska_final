9f5751e66cf1db04b2c2024a2eae2b4923b66412
SandiHorvat <sandi.horvat2@gmail.com>
2019-07-11 21:29:24 +0200

    Migriranje na AndroidX v xml view datotekah.Dodana 2 gumba(zaenkrat brez listenerjev ter povezave kode, samo mockup): gumb za informacijo o senzorju in gumb za pošiljanje. Dodani stringi za imena gumbov Zakomentiran javanski test file, ne potreben, povzočal napake.

--- mobile/src/androidTest/java/com/diplomska/diplomska_wear_phone_2/ExampleInstrumentedTest.java
+++ mobile/src/androidTest/java/com/diplomska/diplomska_wear_phone_2/ExampleInstrumentedTest.java
@@ -1,19 +1,18 @@
 package com.diplomska.diplomska_wear_phone_2;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
-
+/*
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
@@ -24,3 +23,4 @@ public class ExampleInstrumentedTest {
         assertEquals("com.diplomska.diplomska_wear_phone_2", appContext.getPackageName());
     }
 }
+*/
